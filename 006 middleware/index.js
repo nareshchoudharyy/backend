61c53b548b826e83bf2e5ca485edb84bbafd418a
Naresh Choudhary <nc072113@gmail.com>
2024-06-04 16:53:10 +0530

    hello

--- 006 middleware/index.js
+++ 006 middleware/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
-const tokenCheck =(req,res,next)=>{
-    console.log('token checked')
-    next();
-}
 
-app.get('/user',tokenCheck,(req,res)=>{
-    res.send('data fetched successfully')
+const token = "nc123";
+const tokenCheck =(req,res,next)=>{
+    const usertoken = req.params.val;
+    if(!usertoken){
+        res.status(400).json({msg:'please Provide token Id'})
+    }
+    else if(usertoken != token){
+        res.status(401).json({msg:'Invalid token Id'})
+    } else{
+        next();
+    }
+}
+// app.use(tokenCheck)
+app.get('/',(req,res)=>{
+    res.send('at home page')
+})
+app.get('/user/:val?',tokenCheck,(req,res)=>{
+    res.send('data fetched successfully for user')
+})
+app.get('/admin:val?',tokenCheck,(req,res)=>{
+    res.send('data fetched successfully for admin')
+})
+app.get('/dev:val?',tokenCheck,(req,res)=>{
+    res.send('data fetched successfully for developer')
 })
 app.listen(5000,()=>{
     console.log('task done successfully')
--- 007 route-level-middleware/index.js
+++ 007 route-level-middleware/index.js
@@ -1,8 +1,75 @@
 const express = require('express');
 const app = express();
+const router1 = express.Router();
+const router2 = express.Router();
+const router3 = express.Router();
+
+const m1=(req,res,next)=>{
+    console.log('at m1 middleware for router1 using services as its route')
+    next();
+}
+router1.use(m1);
+router1.get('/',(req,res)=>{
+    res.send('at services (get)')
+})
+router1.post('/',(req,res)=>{
+    res.send('at services (post)')
+})
+router1.put('/',(req,res)=>{
+    res.send('at services (put)')
+})
+router1.patch('/',(req,res)=>{
+    res.send('at services (patch)')
+})
+router1.delete('/',(req,res)=>{
+    res.send('at services (delete)')
+})
+
+const m2=(req,res,next)=>{
+    console.log('at m2 middleware for router2 using services as its route')
+    next();
+}
+router2.use(m2);
+router2.get('/',(req,res)=>{
+    res.send('at gallery file (get)')
+})
+router2.post('/',(req,res)=>{
+    res.send('at gallery file (post)')
+})
+router2.put('/',(req,res)=>{
+    res.send('at gallery file (put)')
+})
+router2.patch('/',(req,res)=>{
+    res.send('at gallery file (patch)')
+})
+router2.delete('/',(req,res)=>{
+    res.send('at gallery file (delete)')
+})
+
+const m3=(req,res,next)=>{
+    console.log('at m3 middleware for router3 using services as its route')
+    next();
+}
+router3.use(m3);
+router3.get('/',(req,res)=>{
+    res.send('at router3 file (get)')
+})
+router3.post('/',(req,res)=>{
+    res.send('at router3 file (post)')
+})
+router3.put('/',(req,res)=>{
+    res.send('at router3 file (put)')
+})
+router3.patch('/',(req,res)=>{
+    res.send('at router3 file (patch)')
+})
+router3.delete('/',(req,res)=>{
+    res.send('at router3 file (delete)')
+})
+
 
 const tokenCheck=(req,res,next)=>{
-    console.log('at Middleware function')
+    console.log('At Middleware function-App')
     next();
 }
 app.use(tokenCheck);
@@ -13,6 +80,11 @@ app.get('/',(req,res)=>{
 app.post('/',(req,res)=>{
     res.send('at home page using app.post');
 })
-app.listen("5000",()=>{
+
+app.use('/services', router1);
+app.use('/gallery', router2);
+app.use('/router3', router3);
+
+app.listen(5000,()=>{
     console.log('Website is running at Port:- 5000')
 })
